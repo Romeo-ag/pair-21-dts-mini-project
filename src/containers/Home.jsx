5fd60e8257b4949fc37cbc71a7b6adc9ffa6dc94
Muhammad Fadilah Arfat <fadilitas14@gmail.com>
2022-07-21 10:51:43 +0700

    fadil: fix bug

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
 import styles from "./Home.module.css";
-import { auth } from "../authentication/firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
 
 const Home = () => {
-  const [user] = useAuthState(auth);
   return (
     <>
       <Box className={styles.container}>
